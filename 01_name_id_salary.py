890f057e7ff4c077c85defd8925f5d647c0a7158
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-12-03 21:49:48 +0530

    Add files via upload

---
+++ 01_name_id_salary.py
@@ -0,0 +1,10 @@
+'''Printing the name, id and salary from the user'''
+
+Name= input("Enter your name:")
+
+Id= int(input("Enter your Id number:"))
+
+Salary= float(input("Enter your Salary:"))
+
+print(f"Your name is: {Name}, Your Id is: {Id}, Your salary is:{Salary}")
+
