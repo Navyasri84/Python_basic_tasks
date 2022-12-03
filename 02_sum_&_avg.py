4395f3fbe62d289ae5d40582ab3d080d8fa02055
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-12-03 21:51:40 +0530

    Add files via upload

---
+++ 02_sum_&_avg.py
@@ -0,0 +1,9 @@
+'''sum and avg of 2 numbers from the user input'''
+
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+
+sum=a+b
+Avg= (sum)/2
+print("The sum of two numbers is:",sum)
+print("The avg of two numbers is:",Avg)
