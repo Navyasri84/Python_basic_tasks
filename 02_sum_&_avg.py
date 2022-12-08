df064bd445dc2fb27950534800c1ea405cc03179
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-12-08 19:15:20 +0530

    Add files via upload

---
+++ 02_sum_&_avg.py
@@ -0,0 +1,18 @@
+'''sum and avg of 2 numbers from the user input'''
+
+'''............first way.........'''
+
+# a=float(input("Enter first number:"))
+# b=float(input("Enter second number:"))
+
+# sum=a+b
+# Avg= (sum)/2
+# print("The sum of two numbers is:",sum)
+# print("The avg of two numbers is:",Avg)
+
+'''............second way.........'''
+
+# a,b=[float(x) for x in input("Enter 2 Numbers:").split(',')]
+# sum=a+b
+# avg=sum/2
+# print("The Sum is %.2f\nAverage is %.2f" %(sum,avg))
