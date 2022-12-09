b9cb0c0ed0fea0fa287d6bcbaf753a9d02c6ec21
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-12-09 19:51:48 +0530

    Add files via upload

---
+++ 06_cube_number.py
@@ -0,0 +1,13 @@
+'''cubic of any number'''
+
+'''............first way.........'''
+
+a= eval(input("Enter any number:"))
+print("Cubic of "+str(a)+" is,",a**3)
+
+'''............second way.........'''
+
+import math
+a=eval(input("Enter any number:"))
+print(math.pow(a,3))
+
