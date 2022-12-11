7e665fed029a4330b5992258f9003fd4e30eb74a
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-12-11 16:45:54 +0530

    Add files via upload

---
+++ 09_even_odd.py
@@ -0,0 +1,28 @@
+'''using if_else'''
+
+# a=int(input("Enter any number:"))
+
+# if a%2==0:
+#     print("You entered number is Even")
+# else:
+#     print("You entered number is Odd")
+
+
+'''using for_loop'''
+
+# n=int(input("Enter a number:"))
+# for i in range(0,n,2):
+#     print(i)
+
+# n=int(input("Enter a number:"))
+# for i in range(1,n,2):
+#     print(i)
+
+
+'''using while_loop'''
+a=int(input("enter starting number:"))
+b=int(input("enter ending number:"))
+while(a<=b):
+    print(a)
+    a+=2
+
