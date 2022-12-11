db22de1dde5b001f401f20ab0555dce1fc45649e
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-12-11 16:46:21 +0530

    Add files via upload

---
+++ 10_even_odd_zero.py
@@ -0,0 +1,10 @@
+a=int(input("Enter any number:"))
+
+if (a==0):
+    print("You entered number is Zero")
+
+elif(a%2==0):
+    print("You entered number is Even")
+
+else:
+    print("You entered number is Odd")
