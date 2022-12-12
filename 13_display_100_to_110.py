c3ceaa2e0c15724da0b89b44ecaa82684d825220
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-12-12 17:23:03 +0530

    Add files via upload

---
+++ 13_display_100_to_110.py
@@ -0,0 +1,11 @@
+'''using for_loop'''
+
+for i in range(100,111):
+    print(i)
+
+'''using while_loop'''
+m=int(input("Enter the starting even number:"))
+n=int(input("Enter the ending even number:"))
+while(m<=n):
+    print(m)
+    m+=2
