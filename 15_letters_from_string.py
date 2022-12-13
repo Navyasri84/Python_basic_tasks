1adc544afd20f8bdb20e505e97ec74a3d7addbca
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-12-13 15:19:54 +0530

    Add files via upload

---
+++ 15_letters_from_string.py
@@ -0,0 +1,15 @@
+name="python"
+for i in name:
+    print(i)
+
+'''in reverse order'''
+
+name="python"
+for i in name[::-1]:
+    print(i)
+
+'''in alternative'''
+
+name="python"
+for i in name[::2]:
+    print(i)
